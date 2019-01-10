be9f65bbcc7696a9b6f88af9d9d073819f22f093
Laura Colten <lauracolten5@gmail.com>
2019-01-09 21:41:00 -0600

    Added index.js

--- public/index.js
+++ public/index.js
@@ -1,12 +1,33 @@
-document.getElementById("main").innerHTML = products.map((item, index)=> {
-    return `<img src="${item.imgUrl}" />`
-    // return '<img src="' + item.imgUrl + '">'
-    // return "<img src='" + item.imgUrl + "'>"
-}).join('')
+const getEverything = (list) => {
+    document.getElementById("main").innerHTML = list.map((item, index) => {
+        return `
+            <div>
+                <img src="${item.imgUrl}" />
+                <h3>Product Name:${item.name}</h3>
+                <p>Rating: ${item.rating}</p>
+                <p>Number of Reviews: ${item.numReviews}</p>
+                <p>Price: ${item.price}</p>
+                <a href="#" class="showDetails">Details</a>
+                <div style="display: none">${item.description}</div>
+            </div>
+     `;
+    }).join('')
+}
+getEverything(products);
+document.querySelectorAll(".showDetails").forEach(element => {
+    element.addEventListener("click", event => {
+        event.preventDefault();
+        event.target.nextElementSibling.style.display = "block";
+    })
+});
 
-'<h1>Hello!</h1>'
-"<h1>Hello!</h1>"
+document.querySelector("[type='submit']").addEventListener("click", e => {
+    e.preventDefault();
+    const searchValue = document.querySelector("[type='text']").value;
+    console.log(searchValue);
+    const filteredProducts = products.filter(p => {
+        return p.name.includes(searchValue);
+    })
+    getEverything(filteredProducts);
+})
 
-const name = 'Laura';
-
-`<h1>${name}</h1>`
